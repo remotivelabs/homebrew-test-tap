91d89706faf5617123d365486c8825c703c81b9b
Johan Rask <johan.rask@remotivelabs.com>
2023-09-13 13:33:16 +0200

    New feature

---
+++ Formula/v0.3.0.rb
@@ -0,0 +1,17 @@
+class V030 < Formula
+  desc "Some Desc"
+  homepage "https://example.com"
+  url "https://github.com/psampaz/gothanks/archive/v0.3.0.tar.gz"
+  sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
+  license ""
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system "false"
+  end
+end
